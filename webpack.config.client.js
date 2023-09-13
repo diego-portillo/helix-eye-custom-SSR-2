c5e8f14df97b02642df44eb020bb4e4568bffd1d
Diego Portillo <djportillo@taxit.com.py>
2023-09-13 17:52:29 -0400

    clase 15: editar manualmente archivo .env

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,5 +1,6 @@
+const { HotModuleReplacementPlugin} = require('webpack')
+
 const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -7,10 +8,15 @@ dotenv.config()
 const mode = process.env.NODE_ENV ?? 'production'
 const isDev = process.env.NODE_ENV !== 'production'
 const PORT = process.env.PORT
-
+let plugins =[]
+let entries = ['./src/app/index.tsx']
+if(isDev){
+  entries.unshift('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true')
+  plugins.push(new HotModuleReplacementPlugin())
+}
 module.exports = {
   name: 'client',
-  entry: './src/app/index.tsx',
+  entry: entries ,
   mode,
   devtool: isDev ? 'eval-source-map' : undefined,
   stats: 'errors-only',
@@ -50,11 +56,7 @@ module.exports = {
       },
     ]
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './src/public/index.html',
-    })
-  ],
+  plugins,
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
